16da9267daadf9adc9f687b1d2c612734be8c30e
Liesse205 <liessekm@gmail.com>
2025-11-13 18:10:41 +0000

    Sync LeetCode submission - Time Needed to Buy Tickets (python3)

---
+++ problems/time_needed_to_buy_tickets/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        time = 0
+        target_tickets = tickets[k]
+        for i in range(len(tickets)):
+            if i<=k:
+                time += min(tickets[i], target_tickets)
+            else:
+                time += min(tickets[i], target_tickets - 1)
+        return time    
+        
