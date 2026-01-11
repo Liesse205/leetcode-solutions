3d72a868facf116ca2596638d9df816f93920547
Liesse205 <liessekm@gmail.com>
2026-01-11 22:50:38 +0000

    Sync LeetCode submission - Apply Operations to an Array (python3)

--- problems/apply_operations_to_an_array/solution.py
+++ problems/apply_operations_to_an_array/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums) - 1):
-                if nums[i] == nums[i + 1]:
-                    nums[i] *= 2
-                    nums[i + 1] = 0 
-        non_zeros = [x for x in nums if x != 0]
-        zeros = [0] * (len(nums) - len(non_zeros))
-        return(non_zeros + zeros) 
+        n = len(nums)
+        for i in range(0, n-1):
+            if nums[i] == nums[i+1]:
+                nums[i] = nums[i]*2
+                nums[i+1] = 0
+        non_zeros = [x for x in nums if x!= 0]
+        zeros = [y for y in nums if y == 0]
+        return(non_zeros + zeros)
         
-               
