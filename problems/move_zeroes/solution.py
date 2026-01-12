6bce95eaf89e7a63366ff64bc2a2bdb4dc839a89
Liesse205 <liessekm@gmail.com>
2026-01-12 20:10:30 +0000

    Sync LeetCode submission - Move Zeroes (python3)

--- problems/move_zeroes/solution.py
+++ problems/move_zeroes/solution.py
@@ -5,10 +5,7 @@ class Solution:
         """
         for i in range(len(nums)):
             for j in range (i+1, len(nums)):
-                if nums[i]== 0 and nums[j] != 0:
-                    nums[i], nums[j] = nums[j], nums[i]
-                    
-                
+                if nums[i] == 0 and  nums[j]!= 0:
+                    nums[i],nums[j] = nums[j],nums[i]
 
-            
         
